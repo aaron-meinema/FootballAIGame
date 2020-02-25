10544e09403476d8bb405d8c49928ad40837beed
aaron <aaronwillem@hotmail.com>
2020-02-25 17:33:04 +0100

    initial commit

---
+++ FootballAIGame/Source/Engine/Basic2D.cs
@@ -0,0 +1,44 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace FootballAIGame
+{
+    public class Basic2D
+    {
+        public Vector2 pos, dims;
+        public Texture2D mySprite;
+
+        public Basic2D(string path, Vector2 pos, Vector2 dims)
+        {
+            this.pos = pos;
+            this.dims = dims;
+            mySprite = Globals.content.Load<Texture2D>(path);
+        }
+        
+        public void Update() {
+
+        }
+
+        public void Draw() {
+            if(mySprite != null) {
+                //these 3 lines are 1 line
+                Globals.spriteBatch.Draw(mySprite, new Rectangle((int)pos.X, (int)pos.Y, (int)dims.X, (int)dims.Y),
+                    null, Color.White, 0.0f, new Vector2(mySprite.Bounds.Width / 2, mySprite.Bounds.Height / 2), 
+                    new SpriteEffects(), 0);
+            }
+        }
+    }
+}
---
+++ FootballAIGame/Source/Engine/World.cs
@@ -0,0 +1,35 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace FootballAIGame {
+
+    public class World {
+
+        public Basic2D playerOne;
+
+        public World() {
+            playerOne = new Basic2D("2d/sprite", new Vector2(300, 300), new Vector2(48, 48));
+        }
+
+        public virtual void Update() {
+            playerOne.Update();
+        }
+
+        public virtual void Draw() {
+            playerOne.Draw();
+        }
+    }
+}
